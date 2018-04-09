adf7ccb13072e0536ca97bc342a71152ba68f31f
github username <github email address>
2018-04-09 17:09:58 -0500

    building out views and routes for signup and user authentication and customer index.erb

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,11 +1,19 @@
 class CustomersController < ApplicationController
     
-    get 'registrations' do
-        erb :"registrations/signup"
+    get '/registrations/signup' do
+        erb :"/registrations/signup"
     end
 
+    get '/customers/:id' do
+        erb :"customers/#{@customer.id}/show"
+    end
+
+
     post 'registrations' do
-        erb :"/"
+        @customer = Customer.create(name: params["name"], email: params["email"], password: params["password"])
+        @customer.save
+        session[:id] = @customer.id
+        redirect '/customers/:id/show'
     end
 
 

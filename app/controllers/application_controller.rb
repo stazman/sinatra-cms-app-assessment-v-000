f39103e6ddd8d6cc195a8cd25dd03bc62119939c
github username <github email address>
2018-04-09 11:47:50 -0500

    made changes to application_controller and environment.rb configuration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,16 @@
 class ApplicationController < Sinatra::Base
     register Sinatra::ActiveRecordExtension
+    
+    enable :sessions
+    
     set :session_secret, "my_secret"
     set :views, Proc.new { File.join(root, "../views/") }
-      
+     
+    configure do
+        set :public_folder, 'public'
+        set :views, 'app/views'
+    end
+
     get '/' do
         erb :index
     end
--- config/environment.rb
+++ config/environment.rb
@@ -3,11 +3,7 @@ Bundler.require
 
 ENV['SINATRA_ENV'] ||= "development"
 
-configure :development do
-    set :database, 'sqlite3:db/sbw.db'
-end
-  
-ActiveRecord::Base.establish_connection(
+# ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/sbw#{ENV['SINATRA_ENV']}.sqlite"
 )

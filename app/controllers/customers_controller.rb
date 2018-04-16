be9e83339a61379962ce1c6a3a0fa3718343b34e
github username <github email address>
2018-04-16 18:46:29 -0500

    building out views and stubbing their functionality

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,11 +1,22 @@
+require 'pry'
+
 class CustomersController < ApplicationController
-    
+
     get '/customers/new' do
         erb :'/customers/new'
-    end 
+    end
 
     post '/customers/new' do
         @customer = Customer.create(customer_first_name: params["customer_first_name"], customer_last_name: params["customer_last_name"], email: params["email"], password: params["password"], address: params["address"], phone_1: params["phone_1"], phone_2: params["phone_2"], fax: params["fax"])
         @customer.save
     end
+
+    get '/customers/login' do
+        
+    end
+
+    get '/customers/:id' do
+    end
+
+
 end

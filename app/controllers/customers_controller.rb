32538439db1e8c7c744735f3ca48d97ec63d0996
github username <github email address>
2018-04-16 17:26:51 -0500

    building out customers_controller and customers/new view

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -5,7 +5,7 @@ class CustomersController < ApplicationController
     end 
 
     post '/customers/new' do
-        @customer = Customer.create(customer_first_name: params("customer_first_name"), customer_last_name: params("customer_last_name"), email: params("email"), password: params("password"), address: params("address"), phone_1: params("phone_1"), phone_2: params("phone_2"), fax: params("fax")
+        @customer = Customer.create(customer_first_name: params["customer_first_name"], customer_last_name: params["customer_last_name"], email: params["email"], password: params["password"], address: params["address"], phone_1: params["phone_1"], phone_2: params["phone_2"], fax: params["fax"])
         @customer.save
     end
 end

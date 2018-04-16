ec2905712cecdcd202ebb6fa7ac318d75d413e98
github username <github email address>
2018-04-16 17:13:36 -0500

    building CustomersController

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,10 +1,11 @@
 class CustomersController < ApplicationController
     
-    get '/customers/new' 
-        @customer = Customer.create()
+    get '/customers/new' do
         erb :'/customers/new'
     end 
 
-
-
+    post '/customers/new' do
+        @customer = Customer.create(customer_first_name: params("customer_first_name"), customer_last_name: params("customer_last_name"), email: params("email"), password: params("password"), address: params("address"), phone_1: params("phone_1"), phone_2: params("phone_2"), fax: params("fax")
+        @customer.save
+    end
 end

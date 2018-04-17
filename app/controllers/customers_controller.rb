efe7bddf0d18527c026efb1a2179439a90165029
github username <github email address>
2018-04-17 15:16:30 -0500

    building CustomersController

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,20 +1,23 @@
 class CustomersController < ApplicationController
 
-    get '/customers/new' do
-        erb :'/customers/new'
-    end
-
     get '/customers/login' do
-        
+        @customer = Customer.find_by_id(params[:id])
+        @orders = Order.where(customer_id: @customer.id)
+        erb :"/customers/show_customer_info" 
     end
 
-    get '/customers/signup' do
+    get '/customers/customer_signup' do
+        if logged_in?
+            redirect to '/customers/all_customer_orders'
+        else
+            erb :'/customers/customer_signup'
+        end      
     end
 
     get '/customers/:id' do
     end
 
-    post '/customers/new' do
+    post '/customers' do
         @customer = Customer.create(customer_first_name: params["customer_first_name"], customer_last_name: params["customer_last_name"], email: params["email"], password: params["password"], address: params["address"], phone_1: params["phone_1"], phone_2: params["phone_2"], fax: params["fax"])
         @customer.save
         redirect '/admin/customers_and_orders'

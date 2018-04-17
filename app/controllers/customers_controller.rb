9e2c46301a3500c5baa0697ee90858f95963dd4a
github username <github email address>
2018-04-17 18:45:00 -0500

    getting customer controllers to work with views

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,27 +1,36 @@
+require 'pry'
+
 class CustomersController < ApplicationController
 
-    get '/customers/login' do
-        @customer = Customer.find_by_id(params[:id])
-        @orders = Order.where(customer_id: @customer.id)
-        erb :"/customers/show_customer_info" 
-    end
+
+    #  get '/customers/:id' do
+    # #     # how can it go to id here ... and at the top of the blocks? or does position matter??
+    #     @customer = Customer.find(params[:id])
+    # #     # @orders = Order.where(customer_id: @customer.id)
+    #     erb :"/customers/show_customer_info" 
+    # end
+
+    # get '/customers/login' do
+    #     @customer = Customer.find_by_id(params[:id])
+    #     @orders = Order.where(customer_id: @customer.id)
+    #     erb :"/customers/show_customer_info" 
+    # end
 
     get '/customers/customer_signup' do
-        if logged_in?
-            redirect to '/customers/all_customer_orders'
-        else
+
+        # if logged_in?
+        #     redirect to '/customers/all_customer_orders'
+        # else
             erb :'/customers/customer_signup'
-        end      
     end
 
-    get '/customers/:id' do
-        erb :'/customers/:id/all_customer_orders'
-    end
+    # get '/customers/:id' do
+    #     erb :'/customers/:id/all_customer_orders'
+    # end
 
-    post '/customers' do
+    post '/customers/customer_signup' do
         @customer = Customer.create(first_name: params["first_name"], last_name: params["last_name"], email: params["email"], password: params["password"], address: params["address"], phone_1: params["phone_1"], phone_2: params["phone_2"], fax: params["fax"])
         @customer.save
-        redirect '/customers/:id/all_customer_orders'
+        redirect '/customers/show_customer_info'
     end
-
 end    

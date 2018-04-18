66a41145bff2f6d4e20460a6bce2a9be16c62945
github username <github email address>
2018-04-18 02:32:12 -0400

    made sure database is accepting and making records with signup data; got show_customer_info to render

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -3,34 +3,26 @@ require 'pry'
 class CustomersController < ApplicationController
 
 
-    #  get '/customers/:id' do
-    # #     # how can it go to id here ... and at the top of the blocks? or does position matter??
-    #     @customer = Customer.find(params[:id])
-    # #     # @orders = Order.where(customer_id: @customer.id)
-    #     erb :"/customers/show_customer_info" 
-    # end
-
-    # get '/customers/login' do
-    #     @customer = Customer.find_by_id(params[:id])
-    #     @orders = Order.where(customer_id: @customer.id)
-    #     erb :"/customers/show_customer_info" 
-    # end
-
-    get '/customer_signup' do
-        if logged_in?
-            redirect to '/customers/show_customer_info'
-        else
-            erb :'/customer_signup'            
-        end
+     get '/customers/customer_signup' do
+        
+        erb :"/customers/customer_signup"
     end
+
+    # get '/customer_signup' do
+    #     if logged_in?
+    #         redirect to '/customers/show_customer_info'
+    #     else
+    #         erb :'/customer_signup'            
+    #     end
+    # end
 
     # get '/customers/:id' do
     #     erb :'/customers/:id/all_customer_orders'
     # end
 
     post '/customers/customer_signup' do
-        @customer = Customer.create(first_name: params["first_name"], last_name: params["last_name"], email: params["email"], password: params["password"], address: params["address"], phone_1: params["phone_1"], phone_2: params["phone_2"], fax: params["fax"])    
+        @customer = Customer.create(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], password: params[:password], address: params[:address], phone_1: params[:phone_1], phone_2: params[:phone_2], fax: params[:fax])    
         @customer.save
-        redirect '/customers/show_customer_info'
+        erb :'/customers/show_customer_info'
     end
 end 

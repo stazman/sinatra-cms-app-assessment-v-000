acad34b9bfbbcd523042f1504b26fd58f9656efa
github username <github email address>
2018-04-18 16:12:56 -0400

    debugging for login controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -3,26 +3,25 @@ require 'pry'
 class CustomersController < ApplicationController
 
 
+
+    
     get '/customers/customer_signup' do      
         erb :"/customers/customer_signup"
     end
 
-    get '/customers/edit_customer_info' do
-        erb :"/customers/edit_customer_info"
-    end
-
     get '/customers/customer_login' do
+        @customer = Customer.find(params[:id])
         if logged_in?
-            erb :'/customers/customer_info_and_orders'
+            redirect '/customers/:id'
         else
             erb :"/customers/customer_login"
         end
     end
 
-    # get '/customers/:id' do
-    #     @customer = Customer.find(params[:id])
-    #     erb :'/customers/customer_info_and_orders'
-    # end
+    get '/customers/:id' do
+        @customer = Customer.find(params[:id])
+        erb :'/customers/customer_info_and_orders'
+    end
 
     get '/customers/edit_customer_info' do
         erb :'/customers/edit_customer_info'
@@ -32,7 +31,8 @@ class CustomersController < ApplicationController
         @customer = Customer.create(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], password: params[:password], address: params[:address], phone_1: params[:phone_1], phone_2: params[:phone_2], fax: params[:fax])    
         @customer.save
         session[:user_id] = @customer.id
-        erb :'/customers/customer_info_and_orders'
+        redirect '/customers/#{@customer.id}' 
+        # ??? How is the interpolated string read though it is in a string already
     end
 
     post '/customers/customer_login' do

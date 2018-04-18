e91cbe5165b780955d07f1ecb8a1ce13c845d6bf
github username <github email address>
2018-04-18 01:46:33 -0400

    getting customer controllers to work with views

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -16,12 +16,12 @@ class CustomersController < ApplicationController
     #     erb :"/customers/show_customer_info" 
     # end
 
-    get '/customers/customer_signup' do
-
-        # if logged_in?
-        #     redirect to '/customers/all_customer_orders'
-        # else
-            erb :'/customers/customer_signup'
+    get '/customer_signup' do
+        if logged_in?
+            redirect to '/customers/show_customer_info'
+        else
+            erb :'/customer_signup'            
+        end
     end
 
     # get '/customers/:id' do
@@ -29,8 +29,8 @@ class CustomersController < ApplicationController
     # end
 
     post '/customers/customer_signup' do
-        @customer = Customer.create(first_name: params["first_name"], last_name: params["last_name"], email: params["email"], password: params["password"], address: params["address"], phone_1: params["phone_1"], phone_2: params["phone_2"], fax: params["fax"])
+        @customer = Customer.create(first_name: params["first_name"], last_name: params["last_name"], email: params["email"], password: params["password"], address: params["address"], phone_1: params["phone_1"], phone_2: params["phone_2"], fax: params["fax"])    
         @customer.save
         redirect '/customers/show_customer_info'
     end
-end    
+end 

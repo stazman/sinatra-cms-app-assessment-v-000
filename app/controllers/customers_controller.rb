11039942389ace795eff283a02d7c5c4260b534b
github username <github email address>
2018-04-18 03:45:20 -0400

    getting customer controllers to work with views

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -3,26 +3,51 @@ require 'pry'
 class CustomersController < ApplicationController
 
 
-     get '/customers/customer_signup' do
-        
+    get '/customers/customer_signup' do      
         erb :"/customers/customer_signup"
     end
 
-    # get '/customer_signup' do
-    #     if logged_in?
-    #         redirect to '/customers/show_customer_info'
-    #     else
-    #         erb :'/customer_signup'            
-    #     end
-    # end
+    get '/customers/edit_customer_info' do
+        erb :"/customers/edit_customer_info"
+    end
 
-    # get '/customers/:id' do
-    #     erb :'/customers/:id/all_customer_orders'
-    # end
+    get '/customers/customer_login' do
+        if logged_in?
+            erb :'/customers/customer_info_and_orders'
+        else
+            erb :"/customers/customer_login"
+        end
+    end
+
+    get '/customers/edit_customer_info' do
+        erb :'/customers/edit_customer_info'
+    end
 
     post '/customers/customer_signup' do
         @customer = Customer.create(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], password: params[:password], address: params[:address], phone_1: params[:phone_1], phone_2: params[:phone_2], fax: params[:fax])    
         @customer.save
-        erb :'/customers/show_customer_info'
+        session[:user_id] = @customer.id
+        erb :'/customers/customer_info_and_orders'
+    end
+
+    post '/customers/customer_login' do
+        @customer = Customer.find_by(email: params[:email])
+        # binding.pry
+         if @customer && @customer.authenticate(params[:password])
+           session[:user_id] = @ucustomer.id
+           redirect to "/customer/customer_info_and_orders"
+         else
+           redirect '/customers/customer_login' 
+         end
+    end
+
+    post '/customers/edit_customer_info' do
+        @customer = Customer.find(params[:id])
+        if logged_in?
+            @customer.update(params[:customer])
+            redirect '/customer/customer_info_and_orders'
+        else
+            redirect '/customers/customer_login'
+        end
     end
 end 

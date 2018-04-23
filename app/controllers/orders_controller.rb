193235b5cbe09b36c20c5a53a0d92ad9222eaeea
github username <github email address>
2018-04-23 16:42:05 -0500

    building orders controller and views

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,7 @@ class OrdersController < ApplicationController
     
     get '/orders/create_order' do
         if logged_in?
+            @kinds_of_service = ["renovation", "repair", "landscaping/hardscaping"]
             erb :'/orders/create_order'
         else
             redirect '/customers/customer_login'
@@ -9,11 +10,12 @@ class OrdersController < ApplicationController
     end
 
     get '/orders/:id' do
+        # @customer = Customer.where(Order.customer_id == @customer.id)
         erb :'/orders/customer_orders'
     end
 
     post '/orders/create_order' do
-        @order = Order.create(kind_of_service: params[:kind_of_service])
+        @order = Order.create(order_date: params[:order_date], kind_of_service: params[:kind_of_service], order_description: params[:order_description])
         @order.save
         erb :'/customers/customer_info_and_orders'
     end

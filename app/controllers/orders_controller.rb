cd90415e3c475af87859e7bd9507f056c1e525ce
github username <github email address>
2018-04-23 20:13:28 -0500

    troubleshooting program from index page and building edit_new_order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,11 +14,26 @@ class OrdersController < ApplicationController
         erb :'/orders/show_new_order'
     end
 
+    get '/customers/:id/edit_customer_info' do
+        @customer = Customer.find(params[:id])
+        erb :'/customers/edit_customer_info'
+    end
+
 
     post '/orders/create_order' do
         @order = Order.create(order_date: params[:order_date], kind_of_service: params[:kind_of_service], order_description: params[:order_description], customer_id: session[:customer_id])
         @order.save
         redirect "/orders/#{@order.id}"
+    end
+
+    patch '/customers/:id' do
+        @customer = Customer.find(params[:id])
+        if logged_in?
+            @customer.update(params[:customer])
+            redirect "/customers/#{@customer.id}/customer_info_and_orders"
+        else
+            redirect '/customers/customer_login'
+        end
     end
 
     # get '/orders/customer_orders' do

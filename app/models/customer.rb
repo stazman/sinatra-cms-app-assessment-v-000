a6dd3acc14474aa79d4740329c7dd730681a0748
github username <github email address>
2018-04-25 10:27:52 -0500

    building validations in Customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,7 @@
 class Customer < ActiveRecord::Base
     has_many :orders
     has_secure_password
+    validates :email, :presence => true,
+                   :uniqueness => true
+                    :format => ???
 end

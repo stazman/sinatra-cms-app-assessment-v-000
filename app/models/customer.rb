17d831430b01de762401073471e8b472fa2af1b7
github username <github email address>
2018-04-25 11:03:14 -0500

    finished validation for uniqueness, presence, and regex-controlled format for email address

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,6 +3,7 @@ class Customer < ActiveRecord::Base
     has_secure_password
     validates :email, presence: true,
                     uniqueness: true,
-                    format: { with: /\A[a-zA-Z]+\z/,
+                    format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
                         message: "must be an email address in correct email format" }
 end
+

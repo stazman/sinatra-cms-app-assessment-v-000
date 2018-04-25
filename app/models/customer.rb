40c72e9346dbc4eda3c2d9f0443e6b94bf95c622
github username <github email address>
2018-04-25 10:47:02 -0500

    building out email validation in Customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,8 @@
 class Customer < ActiveRecord::Base
     has_many :orders
     has_secure_password
-    validates :email, :presence => true,
-                   :uniqueness => true
-                    :format => ???
+    validates :email, presence: true,
+                    uniqueness: true,
+                    format: { with: /\A[a-zA-Z]+\z/,
+                        message: "must be an email address in correct email format" }
 end

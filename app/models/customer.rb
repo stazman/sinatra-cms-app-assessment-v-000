66270ac30ea63b7437c330b1c02ce240d8ed66a3
github username <github email address>
2018-04-26 07:33:13 -0500

    resolving editing email evaluation problem

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,8 +1,10 @@
 class Customer < ActiveRecord::Base
     has_many :orders
     has_secure_password
+    EMAIL_REGEX = /(?!.*@.*@)+[a-z0-9A-Z!#^$%&'*+-\/=?_`{|}~;]+@+([A-Za-z0-9])+.+[a-zA-Z][a-zA-Z]/
     validates :email,
-                    uniqueness: true
+                    uniqueness: true,
+                    :format => EMAIL_REGEX
                     #???This says that if the email is NOT unique, then .errors will be true
                     # format: {with: /(?!.*@.*@)+[a-z0-9A-Z!#^$%&'*+-\/=?_`{|}~;]+@+([A-Za-z0-9])+.+[a-zA-Z][a-zA-Z]/}
                     # format: EMAIL_REGEX, "must be an email address in correct email format" }
